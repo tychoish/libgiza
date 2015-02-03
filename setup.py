9d327e128e327ea0be04149580aec811b56c1a2a
Sam Kleinman <samk@10gen.com>
2015-02-03 16:55:01 -0500

    initial commit and import

---
+++ setup.py
@@ -0,0 +1,30 @@
+import libgiza
+
+from setuptools import setup, find_packages
+
+REQUIRES = [
+    'jinja2', 'pyyaml',
+]
+
+setup(
+    name='giza',
+    maintainer='tychoish',
+    maintainer_email='sam@tychoish.com',
+    description='Build System Toolkit',
+    version=libgiza.__version__,
+    license='Apache 2.0',
+    url='http://github.com/tychoish/libgiza/',
+    packages=find_packages(),
+    test_suite=None,
+    install_requires=REQUIRES,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    )
